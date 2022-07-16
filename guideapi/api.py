6ea7af5f1e6299882d6e508b34816e31571a853a
zhouc <1022590068@qq.com>
2022-07-16 17:33:23 +0800

    feat(api): support different request method for function view

--- guideapi/api.py
+++ guideapi/api.py
@@ -1,40 +1,53 @@
 import inspect
+import os
+from collections import defaultdict
+# TODO @stolenzc after Python3.9 can replace List with list
+from typing import List, Union
 
+from jinja2 import Environment, FileSystemLoader
 from parse import parse
 from webob import Request, Response
 
 
 class API(object):
 
-    def __init__(self):
+    def __init__(self, templates_dir="templates"):
         # url -> function
-        self.routes = {}
-        
-    def route(self, path):
+        self.routes = defaultdict(dict)
+        self.templates_env = Environment(loader=FileSystemLoader(os.path.abspath(templates_dir)))
+
+    def route(self, path: str, methods: Union[str, List[str]] = 'get'):
         """
         decorator for add route
         """
+
         def wrapper(handler):
-            self.add_route(path, handler)
+            self.add_route(path, handler, methods)
             return handler
+
         return wrapper
 
-    def add_route(self, path, handler):
+    def add_route(self, path, handler, methods: Union[str, List[str]] = 'get'):
         """
         add route to router dict
         Args:
             path: request path
             handler: function
+            methods: request method
         Returns:
             None
         Raises:
             AssertionError: if path in router dict
         """
-        # add_route, deal with same path
-        assert path not in self.routes, "Such route already exists"
-        self.routes[path] = handler
+        if isinstance(methods, str):
+            methods = [methods.lower()]
+        if isinstance(methods, list):
+            methods = [method.lower() for method in methods]
+        for method in methods:
+            assert path not in self.routes[method], "Such route already exists"
+            self.routes[method][path] = handler
 
-    def find_handler(self, request_path):
+    def find_handler(self, request_path, request_method):
         """
         find hander from router dict by request path
         Args:
@@ -43,7 +56,7 @@ class API(object):
             handler: function
             named: real path
         """
-        for path, handler in self.routes.items():
+        for path, handler in self.routes[request_method].items():
             # regular expression to find path
             parse_result = parse(path, request_path)
             if parse_result is not None:
@@ -53,7 +66,7 @@ class API(object):
 
     def wsgi_app(self, environ, start_response):
         """
-        request come in, handle request
+        input request, and return the response
         Args:
             environ: wsgi environ
             start_response: request info
@@ -67,22 +80,27 @@ class API(object):
         return response(environ, start_response)
 
     def __call__(self, environ, start_response):
-        self.wsgi_app(environ, start_response)
+        return self.wsgi_app(environ, start_response)
 
     def handle_request(self, request):
-        """请求调度"""
+        """
+        deal with the handler
+        """
 
         response = Response()
-        handler, kwargs = self.find_handler(request.path)
+        request_method = request.method.lower()
+        handler, kwargs = self.find_handler(request.path, request_method)
 
         try:
             if handler is not None:
                 if inspect.isclass(handler):
                     # find method in class handler
-                    handler = getattr(handler(), request.method.lower(), None)
-                    if handler is None: # if method not found, not supported request method
+                    handler = getattr(handler(), request_method, None)
+                    if handler is None:  # if method not found, not supported request method
                         raise AttributeError("Method now allowed", request.method)
-                handler(request, response, **kwargs)
+                handler_response = handler(request, response, **kwargs)
+                if handler_response and isinstance(handler_response, str):
+                    response.text = handler_response
             else:
                 # 返回默认错误
                 self.defalut_response(response)
@@ -95,4 +113,12 @@ class API(object):
         default response, if no handler found, set the response for default response
         """
         response.status_code = 404
-        response.text = "Not Found"
+        response.text = "404 Not Found"
+
+    def template(self, template_name, context=None):
+        """
+        deal with template
+        """
+        if context is None:
+            context = {}
+        return self.templates_env.get_template(template_name).render(**context)
--- guideapi/app.py
+++ guideapi/app.py
@@ -2,8 +2,12 @@ from api import API
 
 app = API()
 
+@app.route("/")
+def hello_world(request, response):
+    response.text = "<h2>Hello, World!</h2>"
+
 # 通过装饰器关联路由和方法
-@app.route("/home")
+@app.route("/home", methods=["POST", "GET"])
 def home(request, response):
     response.text = "This is Home"
 
@@ -18,7 +22,10 @@ class BooksResource(object):
     def get(self, req, resp):
         resp.text = "Books Page"
 
+    def post(self, req, resp):
+        resp.text = 'Create Book'
+
 def handler1(req, resp):
     resp.text = "handler1"
-# 可以直接通过add_route方法添加   
+# 可以直接通过add_route方法添加
 app.add_route("/handler1", handler1)
